293458f332195d6eb38c02cd637d507c2f702f19
Revathiadulla <revathiadulla@gmail.com>
2026-06-05 10:06:51 +0530

    testing mail

--- test_mail_send.php
+++ test_mail_send.php
@@ -3,7 +3,7 @@ require_once 'email_helper.php';
 
 $debug = '';
 $result = sendEmail(
-    'your-personal-email@gmail.com',
+    'imp.webportal2026@gmail.com',
     'Test Email from IMP',
     '<h3>This is a test email from Render</h3>',
     null,
